8926db21f4a94ba377414ab10df4c173adb4db74
Pavel Sliusar <psliusar@scnsoft.com>
2017-01-31 21:15:07 +0300

    view-specific logic moved to view processor

--- processor/src/main/java/com/psliusar/layers/binder/processor/BinderClassHolder.java
+++ processor/src/main/java/com/psliusar/layers/binder/processor/BinderClassHolder.java
@@ -1,10 +1,8 @@
 package com.psliusar.layers.binder.processor;
 
 import android.support.annotation.NonNull;
-import android.view.View;
 
 import com.psliusar.layers.binder.LayerBinder;
-import com.psliusar.layers.binder.processor.view.ParentView;
 import com.psliusar.layers.binder.processor.view.ViewField;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
@@ -13,26 +11,18 @@ import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import javax.lang.model.element.Modifier;
 
 public class BinderClassHolder {
 
-    private static final String PARENT_NAME_FORMAT = "parent_%03d";
-    private static final String METHOD_PARAM_LISTENER = "listener";
-    private static final String METHOD_PARAM_VIEW = "view";
-    private static final String METHOD_VAR_TARGET = "target";
 
     private final String packageName;
     private final String className;
     private final String parentClassName;
 
     private final List<ViewField> viewFields = new ArrayList<>();
-    private final Map<Integer, ParentView> viewParents = new HashMap<>();
 
     private boolean fileWritten;
 
@@ -55,24 +45,7 @@ public class BinderClassHolder {
 
     public void addViewField(@NonNull String fieldName, @NonNull String fieldType, int resId, @NonNull Integer parentResId, boolean clickListener) {
         final ViewField desc = new ViewField(fieldName, fieldType, resId, parentResId, clickListener);
-        getViewParentName(parentResId, METHOD_PARAM_VIEW);
         viewFields.add(desc);
-    }
-
-    @NonNull
-    private String getViewParentName(@NonNull Integer resId, @NonNull String fallback) {
-        if (resId == View.NO_ID) {
-            return fallback;
-        }
-
-        ParentView parent = viewParents.get(resId);
-        if (parent == null) {
-            final String name = String.format(Locale.US, PARENT_NAME_FORMAT, viewParents.size() + 1);
-            parent = new ParentView(resId, name);
-            viewParents.put(resId, parent);
-        }
-
-        return parent.getVarName();
     }
 
     @NonNull
@@ -89,118 +62,11 @@ public class BinderClassHolder {
         builder.superclass(ClassName.bestGuess(parentClassName));
 
         if (!viewFields.isEmpty()) {
-            builder.addMethod(getBindMethod());
-            builder.addMethod(getUnbindMethod());
+            builder.addMethod(ViewFieldProcessor.getBindMethod(packageName, className, viewFields));
+            builder.addMethod(ViewFieldProcessor.getUnbindMethod(packageName, className, viewFields));
         }
 
         return builder.build();
     }
 
-    @NonNull
-    private MethodSpec getBindMethod() {
-        final ClassName viewClass = ClassName.get("android.view", "View");
-        final ClassName targetClass = ClassName.get(packageName, className);
-
-        final MethodSpec.Builder builder = MethodSpec.methodBuilder("bind")
-                .addAnnotation(Override.class)
-                .addModifiers(Modifier.PROTECTED)
-                .addParameter(ParameterSpec
-                        .builder(viewClass.nestedClass("OnClickListener"), METHOD_PARAM_LISTENER)
-                        .addAnnotation(ClassName.get(NonNull.class))
-                        .build())
-                .addParameter(ParameterSpec
-                        .builder(viewClass, METHOD_PARAM_VIEW)
-                        .addAnnotation(ClassName.get(NonNull.class))
-                        .build());
-
-        builder.addStatement(
-                "super.bind($L, $L)",
-                METHOD_PARAM_LISTENER,
-                METHOD_PARAM_VIEW
-        );
-
-        // -> final NextLayer target = (NextLayer) listener;
-        builder.addStatement(
-                "final $T $L = ($T) $L",
-                targetClass,
-                METHOD_VAR_TARGET,
-                targetClass,
-                METHOD_PARAM_LISTENER
-        );
-
-        for (Map.Entry<Integer, ParentView> entry : viewParents.entrySet()) {
-            // -> final View parent_001 = find(View.class, view, R.id.container);
-            builder.addStatement(
-                    "final $T $L = find($L, $L)",
-                    viewClass,
-                    entry.getValue().getVarName(),
-                    METHOD_PARAM_VIEW,
-                    entry.getValue().getResId()
-            );
-        }
-
-        for (ViewField field : viewFields) {
-            final ClassName fieldTypeClass = ClassName.bestGuess(field.getFieldType());
-            final String parent = getViewParentName(field.getParentContainer(), METHOD_PARAM_VIEW);
-
-            // -> target.button = (Button) find(Button.class, parent_001, R.id.button);
-            builder.addStatement(
-                    "$L.$L = ($T) find($L, $L)",
-                    METHOD_VAR_TARGET,
-                    field.getFieldName(),
-                    fieldTypeClass,
-                    parent,
-                    field.getResId()
-            );
-
-            // -> target.button.setOnClickListener(target);
-            if (field.isClickListener()) {
-                builder.addStatement(
-                        "$L.$L.setOnClickListener($L)",
-                        METHOD_VAR_TARGET,
-                        field.getFieldName(),
-                        METHOD_VAR_TARGET
-                );
-            }
-        }
-
-        return builder.build();
-    }
-
-    @NonNull
-    private MethodSpec getUnbindMethod() {
-        final MethodSpec.Builder builder = MethodSpec.methodBuilder("unbind")
-                .addAnnotation(Override.class)
-                .addModifiers(Modifier.PROTECTED)
-                .addParameter(ParameterSpec
-                        .builder(ClassName.get("android.view", "View", "OnClickListener"), METHOD_PARAM_LISTENER)
-                        .addAnnotation(ClassName.get(NonNull.class))
-                        .build());
-
-        builder.addStatement(
-                "super.unbind($L)",
-                METHOD_PARAM_LISTENER
-        );
-
-        final ClassName targetClass = ClassName.get(packageName, className);
-        // -> final NextLayer target = (NextLayer) listener");
-        builder.addStatement(
-                "final $T $L = ($T) $L",
-                targetClass,
-                METHOD_VAR_TARGET,
-                targetClass,
-                METHOD_PARAM_LISTENER
-        );
-
-        for (ViewField field : viewFields) {
-            // -> target.button = null;
-            builder.addStatement(
-                    "$L.$L = null",
-                    METHOD_VAR_TARGET,
-                    field.getFieldName()
-            );
-        }
-
-        return builder.build();
-    }
 }
